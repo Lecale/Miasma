bda1a0bd312f19582ebc018156e49b6bb7fde3fb
Lecale <sivadnai@gmail.com>
2020-03-05 22:25:24 +0100

    Tiny additions

--- Program.cs
+++ Program.cs
@@ -11,14 +11,27 @@ namespace Miasma
             Console.BackgroundColor = ConsoleColor.Black;
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("Miasma has loaded");
-
+            Console.WriteLine("1: Create New Tournament");
+            Console.WriteLine("2: Load Tournament");
+            Console.WriteLine("3: Update Player File");
+            int choice = Listen(3);
+            switch (choice)
+            {
+              case 1: ; break;
+              case 2: ; break;
+              case 3: ; break;
+            }
+            
+            /*
             NimMain();
             IntPtr strPtr = HelloNim(1);
             var str = Marshal.PtrToStringUTF8(strPtr);
             Console.WriteLine(str);
             HelloDim(1);
-            // ZipFile.ExtractToDirectory("allworld_lp.zip","Data");
+            ZipFile.ExtractToDirectory("allworld_lp.zip","Data");
+            */
         }
+
 
         [DllImport("HelloNim.dll")]
         public static extern void NimMain();
@@ -27,5 +40,18 @@ namespace Miasma
         public static extern IntPtr HelloNim(int a);
         [DllImport("HelloNim.dll")]
         public static extern void HelloDim(int a);
+
+        
+    static private int Listen(int choices = 1)
+    {
+        bool loop = true;
+        while(loop)
+        {
+            try{int i = int.Parse(Console.ReadLine());
+            if(i>0 && i<=choices) return i; } catch{}
+        }
+        return 0;
     }
+}
+
 }
--- readme.md
+++ readme.md
@@ -3,3 +3,7 @@ It uses ratings as the basis for pairings, not ranks, and allows you to create b
 Miasma smells of console
 It is essentially the second version of Ire, but written in dotnet core in VisualStudioCode.
 It is also my intention to add a bit of Nim inside the workings.
+
+New Tiebreakers
+Some will be renamed, some will be added
+SODOR and SOR are ratings based and new
